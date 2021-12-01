333f7759cbdf592966d33c3d745b05957d21fd2a
JZsolt <jzsolty2000@gmail.com>
2021-12-01 14:38:47 +0200

    remove gitignore files

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img class="mx-auto" alt="Vue logo" src="../assets/logo.png">
+    <Hero/>
+    <hr class="my-10">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Hero from "@/components/hero";
+
+export default {
+  name: 'Home',
+  components: {
+    Hero,
+    HelloWorld,
+  }
+}
+</script>
